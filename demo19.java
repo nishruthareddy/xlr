d7b792a1d626e0595ff9c3bab36d90369336cd98
nishruthareddy <dyavanishrutha@gmail.com>
2025-01-08 11:39:59 +0530

    Add files via upload

---
+++ demo19.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class demo19 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int nextMultipleOfFive = (number / 5 + 1) * 5;
+        System.out.println("The next multiple of 5 is: " + nextMultipleOfFive);
+        scanner.close();
+    }
+}
