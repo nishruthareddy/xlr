de0abe8f2f4556d78c2cdc5531207e5680b092e5
nishruthareddy <dyavanishrutha@gmail.com>
2025-02-06 12:11:49 +0530

    Add files via upload

---
+++ Demo065.java
@@ -0,0 +1,9 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50, 60};       
+        System.out.print("Array elements: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
