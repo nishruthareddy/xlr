d2044292db7d895a3a58622ee5a51ca80e1fa0f1
nishruthareddy <dyavanishrutha@gmail.com>
2025-02-06 12:16:24 +0530

    Add files via upload

---
+++ Demo066.java
@@ -0,0 +1,9 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50, 60};       
+        System.out.println("Index values of the array elements:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Index " + i + ": " + array[i]);
+        }
+    }
+}
