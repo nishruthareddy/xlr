de7d3a6b5536c1a18240d7069debb515cce028d4
nishruthareddy <dyavanishrutha@gmail.com>
2025-02-06 12:19:55 +0530

    Add files via upload

---
+++ Demo057.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Demo057 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int lastDigit = num % 10;
+        int firstDigit = num;
+
+        while (firstDigit >= 10) {
+            firstDigit /= 10;
+        }
+
+        System.out.println("Sum of first and last digits: " + (firstDigit + lastDigit));
+        sc.close();
+    }
+}
